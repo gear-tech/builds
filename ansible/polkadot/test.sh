851b9e7ec2556e2bf211572022c6f748f8b01085
Shamil <shamil@shamil.ru>
2022-08-03 10:09:48 +0500

    Update scripts and configs

---
+++ ansible/polkadot/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Prerequisites:
+#
+# - Ansible (`brew install ansible`)
+# - Private key in `.ansible_key` file
+# - Inventory in `.ansible_inventory` file:
+#     alice-host name=alice node_key=0000000000000000000000000000000000000000000000000000000000000001
+#     bob-host name=bob bootnode=/dns4/alice-host/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp
+#
+
+set -e
+cd "$(dirname "$0")"
+
+ansible-playbook playbook.yml --inventory-file .ansible_inventory --key-file .ansible_key
